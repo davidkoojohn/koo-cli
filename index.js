2420813af50e247c6dc52dfb66e10bc10c0483bd
davidkoojohn <davidkoojohn@gmail.com>
2018-06-14 11:47:51 +0800

    try download-git-repo

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 const program = require('commander')
+const download = require('download-git-repo')
 
 program
   .version('1.0.0', '-v --version')
   .command('init <name>')
   .action((name) => {
-    console.log('========')
-    console.log(name)
-    // console.log(process.argv)
-    console.log('========')
+    download('davidkoojohn/koo-cli', name, function (err) {
+      console.log(err ? 'Error' : 'Success')
+    })
   })
 
 program.parse(process.argv)
