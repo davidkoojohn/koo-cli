ce3bd7be4fac9ae42c30876dc32ec597ccb65c9e
davidkoojohn <davidkoojohn@gmail.com>
2018-06-14 11:57:10 +0800

    try inquirer

--- index.js
+++ index.js
@@ -2,13 +2,23 @@
 
 const program = require('commander')
 const download = require('download-git-repo')
+const inquirer = require('inquirer')
 
 program
   .version('1.0.0', '-v --version')
   .command('init <name>')
   .action((name) => {
-    download('davidkoojohn/koo-cli', name, function (err) {
-      console.log(err ? 'Error' : 'Success')
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'author',
+        message: '请输入作者'
+      }
+    ]).then((answer) => {
+      console.log(answer)
+      download('davidkoojohn/koo-cli', name, function (err) {
+        console.log(err ? 'Error' : 'Success')
+      })
     })
   })
 
